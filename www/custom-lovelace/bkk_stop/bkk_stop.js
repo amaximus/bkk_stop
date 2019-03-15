54445343ed3cc93eb4c53d16294c9aa0c1f001c7
elmerenges <elmerenges@users.noreply.github.com>
2019-03-15 20:21:40 +0100

    Update bkk_stop.js

--- www/custom-lovelace/bkk_stop/bkk_stop.js
+++ www/custom-lovelace/bkk_stop/bkk_stop.js
@@ -1,8 +1,9 @@
-class ContentCardExample extends HTMLElement {
+class BkkStop extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
       const card = document.createElement('ha-card');
-      card.header = 'Example card';
+      card.header = 'BKK';
+      //card.header = state.attributes.stationName;;
       this.content = document.createElement('div');
       this.content.style.padding = '0 16px 16px';
       card.appendChild(this.content);
@@ -12,12 +13,106 @@ class ContentCardExample extends HTMLElement {
     const entityId = this.config.entity;
     const state = hass.states[entityId];
     const stateStr = state ? state.state : 'unavailable';
+    const stationname = state.attributes.stationName;
+    
+    //card.header = state.attributes.stationName;;
+    
+    let bkkObjN = [];
+    var innerstring = `
+    <table width="100%">
+        <th>
+            <td>Járat</td>
+            <td>Cél</td>
+            <td>Perc</td>
+        </th>`;
 
-    this.content.innerHTML = `
-      The state of ${entityId} is ${stateStr}!
-      <br><br>
-      <img src="http://via.placeholder.com/350x150">
+    for (let i = 0; i < state.attributes.items; i++) {
+      let element = {};
+      element.headsign = eval('state.attributes.headsign' + i);
+      element.routeid = eval('state.attributes.routeid' + i);
+      element.rin = eval('state.attributes.in' + i);
+      element.rtype = eval('state.attributes.type' + i);
+      element.wheelchair = 'false';
+      
+      if (state.attributes.hasOwnProperty('wheelchair' + i)) {
+        element.wheelchair = eval('state.attributes.wheelchair' + i);
+      }
+    
+      element.bike = 'false';
+      if (state.attributes.hasOwnProperty('bikesallowed' + i)) {
+        element.bike = eval('state.attributes.bikesallowed' + i);
+      }
+      bkkObjN.push(element);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //[ikon-járat] [kerekesszék] [kerékpár] [cél] [perc]
+    
+    for (let i = 0; i < state.attributes.items; i++) {
+        var jarat = bkkObjN[i].rtype+' '+bkkObjN[i].routeid;
+        innerstring += '<tr><td>'+jarat+'</td><td>'+'</td><td>'+bkkObjN[i].headsign+'</td><td>'+bkkObjN[i].rin+'</td></tr>';
+    }
+    
+    innerstring += '</table>';
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    innerstring += 
+     `<b>${stationname}:</b><br>
+      <ha-icon icon="mdi:bus"></ha-icon> 52 <ha-icon icon="mdi:wheelchair-accessibility"></ha-icon><ha-icon icon="mdi:bike"></ha-icon> P.erzsébet, Pacsirtatelep 9 perc<br>
+      <ha-icon icon="mdi:tram"></ha-icon> 52 <ha-icon icon="mdi:wheelchair-accessibility" class="smallicon"></ha-icon><ha-icon icon="mdi:bike" class="smallicon"></ha-icon> P.erzsébet, Pacsirtatelep 9 perc
+      
+      
     `;
+    
+
+    this.content.innerHTML = innerstring+'</table>';
   }
 
   setConfig(config) {
@@ -28,10 +123,10 @@ class ContentCardExample extends HTMLElement {
   }
 
   // The height of your card. Home Assistant uses this to automatically
-  // distribute all cards over the available columns.
+  // distribute all cards over the available columns. 
   getCardSize() {
     return 3;
   }
 }
 
-customElements.define('content-card-example', ContentCardExample);
+customElements.define('custom-bkk-stop', BkkStop);
